214b3bf2fc234b8df2102efd5033719b7a47d9c5
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-15 22:45:37 +0900

    gameオブジェクト追加

---
+++ backend/src/domain/model/game.py
@@ -0,0 +1,34 @@
+from uuid import UUID, uuid4
+
+from .board import Board
+from .disc import Disc
+
+class Game:
+    def __init__(self, id: UUID, board: Board, current_player: Disc.BLACK | Disc.WHITE):
+        self._id = id
+        self._board = board
+        self._current_player = current_player
+
+    @classmethod
+    def create(cls) -> 'Game':
+        board = Board.create()
+        return cls(uuid4(), board, Disc.BLACK)
+
+    @classmethod
+    def reconstruct(cls, id: UUID, board: Board, current_player: Disc.BLACK | Disc.WHITE) -> 'Game':
+        return cls(id, board, current_player)
+
+    @property
+    def id(self) -> UUID:
+        return self._id
+
+    @property
+    def board(self) -> Board:
+        return self._board
+
+    @property
+    def current_player(self) -> Disc.BLACK | Disc.WHITE:
+        return self._current_player
+
+    def get_valid_moves(self) -> list[tuple[int, int]]:
+        return self.board.get_valid_moves(self.current_player)
