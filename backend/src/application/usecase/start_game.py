724981e2cb85c3194e6dffc9f2d9941968b244f6
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-16 22:47:00 +0900

    start_gameユースケースの返り値を修正

--- backend/src/application/usecase/start_game.py
+++ backend/src/application/usecase/start_game.py
@@ -1,10 +1,16 @@
 from ...domain.repository.game_repository import IGameRepository
 from ...domain.model.game import Game
+from ..dto.start_game import StartGameOutputDTO
 
 class StartGame:
     def __init__(self, game_repository: IGameRepository):
         self.game_repository = game_repository
 
     def execute(self):
-        game = Game.create()
-        return self.game_repository.save(game)
+        new_game = Game.create()
+        new_game = self.game_repository.save(new_game)
+        board_state = [
+            {"row": pos.row, "col": pos.col, "disc": disc.name}
+            for pos, disc in new_game.board.cells.items()
+        ]
+        return StartGameOutputDTO(str(new_game.id), board_state, new_game.current_player.name)
