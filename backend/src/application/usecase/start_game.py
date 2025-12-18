5451cf66c6f7292ea600f7ea040c956ee9a3b465
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-18 23:13:37 +0900

    StartGame修正

--- backend/src/application/usecase/start_game.py
+++ backend/src/application/usecase/start_game.py
@@ -4,13 +4,20 @@ from ..dto.start_game import StartGameOutputDTO
 
 class StartGame:
     def __init__(self, game_repository: IGameRepository):
-        self.game_repository = game_repository
+        self._game_repository = game_repository
 
-    def execute(self):
+    def execute(self) -> StartGameOutputDTO:
         new_game = Game.create()
-        new_game = self.game_repository.save(new_game)
+        self._game_repository.save(new_game)
+
         board_state = [
             {"row": pos.row, "col": pos.col, "disc": disc.name}
             for pos, disc in new_game.board.cells.items()
         ]
-        return StartGameOutputDTO(str(new_game.id), board_state, new_game.current_player.name)
+
+        return StartGameOutputDTO(
+            str(new_game.id),
+            board_state,
+            new_game.current_player.name,
+            new_game.status.value
+        )
