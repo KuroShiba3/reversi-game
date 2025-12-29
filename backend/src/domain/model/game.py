40291a43358bed1798bf444639653d4433007e52
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-30 04:52:12 +0900

    Gameにカスタム例外追加

--- backend/src/domain/model/game.py
+++ backend/src/domain/model/game.py
@@ -1,13 +1,22 @@
 from uuid import UUID, uuid4
 
+from ..exception import CannotPassWithValidMovesException, GameAlreadyFinishedException
 from .board import Board
 from .disc import Disc
 from .game_result import GameResult
 from .game_status import GameStatus
 from .position import Position
 
+
 class Game:
-    def __init__(self, id: UUID, board: Board, current_player: Disc, status: GameStatus, result: GameResult | None = None):
+    def __init__(
+        self,
+        id: UUID,
+        board: Board,
+        current_player: Disc,
+        status: GameStatus,
+        result: GameResult | None = None,
+    ):
         self._id = id
         self._board = board
         self._current_player = current_player
@@ -15,12 +24,14 @@ class Game:
         self._result = result
 
     @classmethod
-    def create(cls) -> 'Game':
+    def create(cls) -> "Game":
         board = Board.create()
         return cls(uuid4(), board, Disc.BLACK, GameStatus.PLAYING)
 
     @classmethod
-    def reconstruct(cls, id: UUID, board: Board, current_player: Disc, status: GameStatus) -> 'Game':
+    def reconstruct(
+        cls, id: UUID, board: Board, current_player: Disc, status: GameStatus
+    ) -> "Game":
         return cls(id, board, current_player, status)
 
     @property
@@ -47,7 +58,9 @@ class Game:
         """現在のプレイヤーの石を指定位置に配置する"""
         self.board.place_disc(position, self._current_player)
         # ターンを次のプレイヤーに切り替え
-        self._current_player = Disc.WHITE if self._current_player == Disc.BLACK else Disc.BLACK
+        self._current_player = (
+            Disc.WHITE if self._current_player == Disc.BLACK else Disc.BLACK
+        )
 
     def get_valid_moves(self) -> list[Position]:
         """現在のプレイヤーの有効な手を取得"""
@@ -57,8 +70,22 @@ class Game:
         return self.board.get_valid_moves(self._current_player)
 
     def pass_turn(self) -> None:
+        """ターンをパスする"""
+        # 終了したゲームではパスできない
+        if self._status == GameStatus.FINISHED:
+            raise GameAlreadyFinishedException(self._id)
+
+        # 有効な手がある場合はパスできない
+        if self.can_current_player_move():
+            valid_moves_count = len(self.get_valid_moves())
+            raise CannotPassWithValidMovesException(
+                self._current_player.name, valid_moves_count
+            )
+
         # ターンを次のプレイヤーに切り替え
-        self._current_player = Disc.WHITE if self.current_player == Disc.BLACK else Disc.BLACK
+        self._current_player = (
+            Disc.WHITE if self.current_player == Disc.BLACK else Disc.BLACK
+        )
 
     def can_current_player_move(self) -> bool:
         """現在のプレイヤーが手を打てるかチェック"""
@@ -83,7 +110,7 @@ class Game:
     def finish(self) -> None:
         """ゲームを終了してGameResultを内部に保持する"""
         if self._status == GameStatus.FINISHED:
-            raise ValueError("ゲームはすでに終了しています。")
+            raise GameAlreadyFinishedException(self._id)
 
         black_score, white_score = self.get_scores()
         self._status = GameStatus.FINISHED
