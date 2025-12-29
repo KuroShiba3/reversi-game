f5944a954d003d5435988aa1fe3f75b02eb05b0e
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-30 04:52:24 +0900

    Positionにカスタム例外追加

--- backend/src/domain/model/position.py
+++ backend/src/domain/model/position.py
@@ -1,17 +1,20 @@
+from ..exception import InvalidPositionException
+
+
 class Position:
     def __init__(self, row: int, col: int):
         if not (0 <= row < 8) or not (0 <= col < 8):
-            raise ValueError("行と列は0から7の範囲内である必要があります。")
+            raise InvalidPositionException(row, col)
 
         self._row = row
         self._col = col
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, Position):
             return False
         return self.row == other.row and self.col == other.col
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.row, self.col))
 
     @property
